fe1a6b7b2a4de1b2b48a3d049d3ebb37b19e5829
Parakeet709 <cogithedog@gmail.com>
2025-02-12 14:23:08 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar">
    <div class="brand">
-      <a href="/"><img src="Parakeet.png" alt="Home">Home</a>
+      <a href="/"><img src="Parakeet.png" alt="Home"></a>
     </div>
     <div class="menu">
        <a href="about.html">Abouts</a>
